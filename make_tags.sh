4bc4e259e69aa2ff34b381362d833c4973bc7ffa
paul <paul@starnex.co.kr>
2018-01-15 11:59:36 +0900

    make_tags에 제외폴더 옵션추가

--- make_tags.sh
+++ make_tags.sh
@@ -11,4 +11,9 @@ rm -rf ./cscope.files ./cscope.out
 find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
 cscope -b -i cscope.files
 
+#ctags -R --c++-kinds=+p --fields=+iaS --extra=+q --exclude=./fp_10016 .
+#rm -rf ./cscope.files ./cscope.out
+#find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -not -path      './fp_10016/*'  -print > cscope.files
+#cscope -b -i cscope.files
+
 #gtags
